534bfabdc284cc08b426c3c2aa7267b42c73a09c
mio2 <motty.mio2@gmail.com>
2021-04-16 13:45:30 +0900

    php 保存時にデータを破損するバグを修正

--- save.php
+++ save.php
@@ -1,6 +1,17 @@
 <?php
-    $json = json_encode(file_get_contents("php://input"), JSON_UNESCAPED_UNICODE);
-    $json = substr(preg_replace('/\\\/u', '', $json), 1, -1);
+    function console_log( $data ){
+        echo '<script>';
+        echo 'console.log('. json_encode( $data ) .')';
+        echo '</script>';
+    }
 
-    file_put_contents("data.json", $json);
+    $raw = file_get_contents("php://input");
+    console_log($raw);
+    if ($raw == "") {
+        console_log("empty");
+    } else {
+        $json = json_encode($raw, JSON_UNESCAPED_UNICODE);
+        $json = substr(preg_replace('/\\\/u', '', $json), 1, -1);
+        file_put_contents("data.json", $json);
+    }
 ?>
